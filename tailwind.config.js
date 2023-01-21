32f4c047a72c0d5ac54c98902c8c380e413914bd
emon1995 <emonhossain995@gmail.com>
2023-01-21 18:13:27 +0600

    Women & man section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        price: "#fabe4c",
+      },
+    },
   },
   plugins: [],
 };
